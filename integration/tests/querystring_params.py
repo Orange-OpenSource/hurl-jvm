0fda694641d6e7e0714869f48a8d5a4ba0bf416e
jcamiel <jeanchristophe.amiel@orange.com>
2020-09-03 16:06:22 +0200

    Add integration tests for commas in query params.

--- integration/tests/querystring_params.py
+++ integration/tests/querystring_params.py
@@ -6,6 +6,7 @@ def querystring_params():
     assert request.args.get('param1') == 'value1'
     assert request.args.get('param2') == ''
     assert request.args.get('param3') == 'a=b'
+    assert request.args.get('param4') == '1,2,3'
     return ''
 
 @app.route("/querystring-params-encoded")
