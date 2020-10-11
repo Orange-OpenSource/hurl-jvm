1c2baa36226407bf0a89bb8770d6237c86be3ec8
jcamiel <jeanchristophe.amiel@orange.com>
2020-10-11 17:35:14 +0200

    Use long opts in dependencies.sh.

--- bin/dependencies.sh
+++ bin/dependencies.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-./gradlew hurl-core:dependencies | grep -v BUILD > docs/dependencies/hurl-core.txt
-./gradlew hurl-cli:dependencies | grep -v BUILD > docs/dependencies/hurl-cli.txt
-./gradlew hurl-fmt:dependencies | grep -v BUILD > docs/dependencies/hurl-fmt.txt
+./gradlew hurl-core:dependencies | grep --invert-match BUILD > docs/dependencies/hurl-core.txt
+./gradlew hurl-cli:dependencies | grep --invert-match BUILD > docs/dependencies/hurl-cli.txt
+./gradlew hurl-fmt:dependencies | grep --invert-match BUILD > docs/dependencies/hurl-fmt.txt
