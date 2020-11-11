de58bdf34b955bc1b41cc9e6a5ef331f785be4b2
jcamiel <jeanchristophe.amiel@orange.com>
2020-11-11 18:09:05 +0100

    Update timeout on gradle publish.

--- bin/publish-maven.sh
+++ bin/publish-maven.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
-./gradlew publish -Dhttp.keepAlive=false -Dmaven.wagon.http.pool=false --info
+./gradlew publish --info -Dorg.gradle.internal.http.socketTimeout=60000 -Dorg.gradle.internal.http.connectionTimeout=60000
